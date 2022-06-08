de99c6b052148753092cf0953ba5e33ccaf97c1c
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2022-06-08 19:08:31 +0200

    GUI: set window size

--- video4discord-egui/src/main.rs
+++ video4discord-egui/src/main.rs
@@ -1,9 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 mod app;
 pub use app::GUI;
+use egui::Vec2;
 
 fn main() {
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        initial_window_size: Some(Vec2::new(800.0, 800.0)),
+        ..eframe::NativeOptions::default()
+    };
     eframe::run_native(
         "eframe template",
         native_options,
